f6bed5d368c6f8a9dab134ed858326a9696baafb
marc-manzanares <marc.manzanares@estudiantat.upc.edu>
2022-09-26 15:57:33 +0200

    Removed Proposer from acceptors code tuple

--- acceptor.erl
+++ acceptor.erl
@@ -13,32 +13,32 @@ init(Name, PanelId) ->
 acceptor(Name, Promised, Voted, Value, PanelId) ->
   receive
     {prepare, Proposer, Round} ->
-      case order:gr(Promised, {Round, Proposer}) of
+      case order:gr(Promised, Round) of
         true ->
-          Proposer ! {promise, {Round, Proposer}, Voted, Value},               
+          Proposer ! {promise, Round, Voted, Value},               
       io:format("[Acceptor ~w] Phase 1: promised ~w voted ~w colour ~w~n",
-                 [Name, {Round, Proposer}, Voted, Value]),
+                 [Name, Round, Voted, Value]),
           % Update gui
           Colour = case Value of na -> {0,0,0}; _ -> Value end,
           PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Voted]), 
-                     "Promised: " ++ io_lib:format("~p", [{Round, Proposer}]), Colour},
-          acceptor(Name, {Round, Proposer}, Voted, Value, PanelId);
+                     "Promised: " ++ io_lib:format("~p", [Round]), Colour},
+          acceptor(Name, Round, Voted, Value, PanelId);
         false ->
           Proposer ! {sorry, {prepare, Round}},
           acceptor(Name, Promised, Voted, Value, PanelId)
       end;
     {accept, Proposer, Round, Proposal} ->
-      case order:goe(Promised, {Round, Proposer}) of
+      case order:goe(Promised, Round) of
         true ->
-          Proposer ! {vote, {Round, Proposer}},
-          case order:goe(Voted, {Round, Proposer}) of
+          Proposer ! {vote, Round},
+          case order:goe(Voted, Round) of
             true ->
       io:format("[Acceptor ~w] Phase 2: promised ~w voted ~w colour ~w~n",
-                 [Name, Promised, {Round, Proposer}, Proposal]),
+                 [Name, Promised, Round, Proposal]),
               % Update gui
-              PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [{Round, Proposer}]), 
+              PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Round]), 
                          "Promised: " ++ io_lib:format("~p", [Promised]), Proposal},
-              acceptor(Name, Promised, {Round, Proposer}, Proposal, PanelId);
+              acceptor(Name, Promised, Round, Proposal, PanelId);
             false ->
               acceptor(Name, Promised, Voted, Value, PanelId)
           end;                            
