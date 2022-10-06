cc77b2841e623a11c6e762bac52fae71a09a31e9
marc-manzanares <marc.manzanares@estudiantat.upc.edu>
2022-10-06 16:06:34 +0200

    last changes

--- acceptor.erl
+++ acceptor.erl
@@ -1,6 +1,7 @@
 -module(acceptor).
 -export([start/2]).
 
+%-define(drop, 6).
 
 start(Name, PanelId) ->
   spawn(fun() -> init(Name, PanelId) end).
@@ -16,6 +17,10 @@ acceptor(Name, Promised, Voted, Value, PanelId) ->
     {prepare, Proposer, Round} ->
       case order:gr(Round, Promised) of
         true ->
+			%P = rand:uniform(10),
+			%if P =< ?drop ->
+			%	io:format("message dropped");	
+			%true ->
 			Proposer ! {promise, Round, Voted, Value},
       io:format("[Acceptor ~w] Phase 1: promised ~w voted ~w colour ~w~n",
                  [Name, Round, Voted, Value]),
@@ -25,12 +30,16 @@ acceptor(Name, Promised, Voted, Value, PanelId) ->
                      "Promised: " ++ io_lib:format("~p", [Round]), Colour},
           acceptor(Name, Round, Voted, Value, PanelId);
         false ->
-          % Proposer ! {sorry, {prepare, Round}},
+          Proposer ! {sorry, {prepare, Round}},
           acceptor(Name, Promised, Voted, Value, PanelId)
       end;
     {accept, Proposer, Round, Proposal} ->
       case order:goe(Round, Promised) of
         true ->
+			%P = rand:uniform(10),
+			%if P =< ?drop ->
+			%	io:format("message dropped");	
+			%true ->
 			Proposer ! {vote, Round},
           case order:goe(Round, Voted) of
             true ->
@@ -44,7 +53,7 @@ acceptor(Name, Promised, Voted, Value, PanelId) ->
               acceptor(Name, Promised, Voted, Value, PanelId)
           end;                            
         false ->
-          % Proposer ! {sorry, {accept, Round}},
+          Proposer ! {sorry, {accept, Round}},
           acceptor(Name, Promised, Voted, Value, PanelId)
       end;
     stop ->
